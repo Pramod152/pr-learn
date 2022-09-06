5386c3033897d830c56f1f5125664ec446c75cf5
rajesh pandey <rajeshpandry24@gmail.com>
2022-09-06 19:24:42 +0545

    new game

--- Rajesh/ludo/script.js
+++
@@ -1,9 +0,0 @@
-const diceRoll1 = () => {
-  const math = Math.trunc(Math.random() * 6) + 1;
-  document.getElementById("dice1").innerHTML = math;
-};
-
-const diceRoll2 = () => {
-  const math = Math.trunc(Math.random() * 6) + 1;
-  document.getElementById("dice2").innerHTML = math;
-};
