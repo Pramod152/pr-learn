bb7901731f6d84f1da30420fa3133ed5c2de55c1
rajesh pandey <rajeshpandry24@gmail.com>
2022-09-06 22:25:52 +0545

    not done

--- Pramod/Project/INC_DEC/script.js
+++ Pramod/Project/INC_DEC/script.js
@@ -3,13 +3,13 @@ var btnMinus = document.querySelector(".minus");
 var btnPlus = document.querySelector(".plus");
 
 console.log(contentc);
-var score = 0;
+var score = 1;
 contentc.textContent = score;
 btnMinus.addEventListener("click", function () {
   if (score >= 1) {
-    contentc.textContent = score--;
+    contentc.textContent = --score;
   } else contentc.textContent = score;
 });
 btnPlus.addEventListener("click", function () {
-  contentc.textContent = score++;
+  contentc.textContent = ++score;
 });
