6f9ac0a4abd85ca9b6a0a5779405320d193b7e8f
Pramod Ghimire <pramodghimire@gmail.com>
2022-09-06 22:13:02 +0545

    new project done

---
+++ Pramod/Project/INC_DEC/script.js
@@ -0,0 +1,15 @@
+var contentc = document.querySelector(".content1");
+var btnMinus = document.querySelector(".minus");
+var btnPlus = document.querySelector(".plus");
+
+console.log(contentc);
+var score = 0;
+contentc.textContent = score;
+btnMinus.addEventListener("click", function () {
+  if (score >= 1) {
+    contentc.textContent = score--;
+  } else contentc.textContent = score;
+});
+btnPlus.addEventListener("click", function () {
+  contentc.textContent = score++;
+});
