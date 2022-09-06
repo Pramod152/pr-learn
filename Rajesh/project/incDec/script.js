3265c8d7c980709c1310223c6d53578bc5d8e788
rajesh pandey <rajeshpandry24@gmail.com>
2022-09-06 23:06:01 +0545

    bug

--- Rajesh/project/incDec/script.js
+++ Rajesh/project/incDec/script.js
@@ -3,16 +3,25 @@ let value = content.innerHTML;
 let incNum = document.querySelector(".btn_up");
 let decNum = document.querySelector(".btn_down");
 
-incNum.addEventListener("click", function () {
-  value++;
-  content.textContent = value;
-});
+// incNum.addEventListener("click", function () {
+//   content.textContent = value++;
+// });
+// decNum.addEventListener("click", function () {
+//   if (value == 0) {
+//     content.textContent = 0;
+//   } else {
+//     content.textContent = value--;
+//   }
+// });
 
-decNum.addEventListener("click", function () {
-  if (value == 0) {
-    content.textContent = 0;
-  } else {
-    value--;
-    content.textContent = value;
+//-------------ON KEY PRESS-------------//
+document.addEventListener("keydown", function (e) {
+  if ((e.key = "ArrowUp")) {
+    content.textContent = value++;
+  }
+});
+document.addEventListener("keydown", function (e) {
+  if ((e.key = "ArrowDown")) {
+    content.textContent = --value;
   }
 });
